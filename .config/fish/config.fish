d1e717aece1e342ab7b455f4ab42f1007a7d37f2
srzh <61797666+88srzh@users.noreply.github.com>
2022-12-19 19:45:20 +0400

    add git commands hotkeys

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-
-    alias wg-up="sudo wg-quick up ubuntu"
-    alias wg-down="sudo wg-quick down ubuntu"
+    alias wg-up="sudo wg-quick up client"
+    alias wg-down="sudo wg-quick down client"
+    alias gs="git status"
+    alias ga="git add ."
+    alias gp="git push"
 end
