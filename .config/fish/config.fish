ab20fb965c89b91e4e1af621e03901048c12ea04
srzh <61797666+88srzh@users.noreply.github.com>
2023-02-24 18:40:49 +0400

    added pomodoro

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,4 +5,14 @@ if status is-interactive
     alias gs="git status"
     alias ga="git add ."
     alias gp="git push"
+
+    function pomodoro 
+  echo $argv[1] | lolcat
+  timer "$argv[2]"m 
+#  spd-say "'$argv[1]' session done"
+end
+
+    alias po="pomodoro"
+    alias wo="pomodoro work 45"
+    alias br="pomodoro break 10"
 end
