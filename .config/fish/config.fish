fab255781b5c9ae48788ab1c7bb2c3acde4e7e3c
srzh <61797666+88srzh@users.noreply.github.com>
2023-03-15 21:13:29 +0400

    commit unnecessary code

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,9 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     alias wg-up="sudo wg-quick up client"
     alias wg-down="sudo wg-quick down client"
-    alias gs="git status"
-    alias ga="git add ."
-    alias gp="git push"
+#    alias gs="git status"
+#    alias ga="git add ."
+#    alias gp="git push"
 
     function pomodoro 
   echo $argv[1] | lolcat
