957d852f56d0ce2f6fa54d98dd86d14bc3868419
srzh <61797666+88srzh@users.noreply.github.com>
2023-06-10 10:58:00 +0400

    added weather

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,9 @@ if status is-interactive
     alias wg-up="sudo wg-quick up client"
     alias wg-down="sudo wg-quick down client"
 
+    # Weather
+    alias weather="curl wttr.in/Izhevsk"
+
     function pomodoro 
   echo $argv[1] | lolcat
   timer "$argv[2]"m 
